afe3a4c7f8d642f54052f3fc4b9d35da31b41917
Hyundo Lee <illhyhl1111@snu.ac.kr>
2016-08-20 07:35:26 +0900

    implement 4
    delete statusArray

--- src/dnf_calculator/Status.java
+++ src/dnf_calculator/Status.java
@@ -1,27 +1,6 @@
 package dnf_calculator;
 //import java.util.HashMap;
 
-class StatusArray
-{
-	public static final int STATNUM = 17;
-	public static final int ELEMENTNUM = 4;
-	public static final String[] statusList =
-		{
-			"str", "intell", "sta", "will",
-			"atk_phy", "atk_mag", "atk_noDef_phy", "atk_noDef_mag",
-			"def_dec_fixed", "def_dec_percent",
-			"dam_inc", "dam_crt", "dam_add",
-			"element_fire","element_water", "element_light","element_darkenss"
-		};
-	
-	//public HashMap<StatusList, Integer> hMap = new HashMap<StatusList, Integer>();
-	/*for(int i=0; i<STATNUM, i++)
-	{
-		hMap.put(i, statusList[i]);
-	}*/
-	
-}
-
 interface StatusList
 {
 	int ELEM_FIRE=0; int  ELEM_WATER=1; int  ELEM_LIGHT=2; int  ELEMT_DARKNESS=3;
@@ -29,6 +8,9 @@ interface StatusList
 	int DEF_DEC_FIXED=8; int  DEF_DEC_PERCENT=9; 
 	int DAM_INC=10; int  DAM_CRT=11; int  DAM_ADD=12;
 	int STR=13; int INTELL=14; int STA=15; int WILL=16;
+	
+	public static final int STATNUM = 17;
+	public static final int ELEMENTNUM = 4;
 }
 
 public class Status {
@@ -37,13 +19,13 @@ public class Status {
 	
 	public Status()
 	{
-		statInfo = new StatusInfo<?>[StatusArray.STATNUM];
+		statInfo = new StatusInfo<?>[StatusList.STATNUM];
 		int i;
-		for(i=0; i<StatusArray.ELEMENTNUM; i++)
-			statInfo[i] = new StatusInfo<StatInfo>(StatusArray.statusList[i], new StatInfo(0));
+		for(i=0; i<StatusList.ELEMENTNUM; i++)
+			statInfo[i] = new StatusInfo<StatInfo>(new StatInfo(0));
 		
-		for(; i<StatusArray.STATNUM; i++)
-			statInfo[i] = new StatusInfo<ElementInfo>(StatusArray.statusList[i], new ElementInfo(false, 0));
+		for(; i<StatusList.STATNUM; i++)
+			statInfo[i] = new StatusInfo<ElementInfo>(new ElementInfo(false, 0));
 	}
 	
 	public void setStatus(Status_Public stat)
@@ -58,13 +40,13 @@ class Status_Public
 	
 	public Status_Public()
 	{
-		statInfo = new StatusInfo<?>[StatusArray.STATNUM];
+		statInfo = new StatusInfo<?>[StatusList.STATNUM];
 		int i;
-		for(i=0; i<StatusArray.ELEMENTNUM; i++)
-			statInfo[i] = new StatusInfo<StatInfo>(StatusArray.statusList[i], new StatInfo(0));
+		for(i=0; i<StatusList.ELEMENTNUM; i++)
+			statInfo[i] = new StatusInfo<StatInfo>(new StatInfo(0));
 		
-		for(; i<StatusArray.STATNUM; i++)
-			statInfo[i] = new StatusInfo<ElementInfo>(StatusArray.statusList[i], new ElementInfo(false, 0));
+		for(; i<StatusList.STATNUM; i++)
+			statInfo[i] = new StatusInfo<ElementInfo>(new ElementInfo(false, 0));
 	}
 	
 	public void setStat(int stat, int strength)
@@ -74,7 +56,7 @@ class Status_Public
 	
 	public void setElementStat(int stat, int strength, boolean activated)
 	{
-		if(StatusArray.ELEMENTNUM>stat) statInfo[stat].setElementStatus(strength, activated);
+		if(StatusList.ELEMENTNUM>stat) statInfo[stat].setElementStatus(strength, activated);
 		else; //Make Error
 	}
 }
@@ -114,11 +96,9 @@ class ElementInfo extends StatInfo
 
 class StatusInfo<T extends StatInfo>
 {
-	private String statName;
 	private T str;
 	
-	public StatusInfo(String name, T strength){
-		statName=name;
+	public StatusInfo(T strength){
 		str=strength;
 	}
 	
@@ -128,7 +108,7 @@ class StatusInfo<T extends StatInfo>
 	
 	public void setElementStatus(int strength, boolean activated)
 	{
-		ElementInfo temp = (ElementInfo)str;
+		if(str instanceof ElementInfo) ElementInfo temp = (ElementInfo)str;
 		temp.setInfo(activated, strength);
 	}
 }
